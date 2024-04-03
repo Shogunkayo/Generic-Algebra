b66d3ce157be2dfee2d14040107024536766ae70
Ganesh Mayya <87609509+thehyena24@users.noreply.github.com>
2024-04-03 12:16:01 +0530

    Added operator overloads to datastructures.h

--- include/datastructures.h
+++ include/datastructures.h
@@ -22,10 +22,39 @@ private:
     U imaginary;
 
 public:
+    ComplexNumber();
     ComplexNumber(T real, U imaginary);
     T getReal();
     U getImaginary();
+    
+    ComplexNumber<T, U> operator+(ComplexNumber<T, U> const& obj)
+    {
+        ComplexNumber<T, U> res;
+        res.real = this->real + obj.real;
+        res.imaginary = this->imaginary + obj.imaginary;
+        return res;
+    }
+    
+    ComplexNumber<T, U> operator-(ComplexNumber<T, U> const& obj)
+    {
+        ComplexNumber<T, U> res;
+        res.real = this->real - obj.real;
+        res.imaginary = this->imaginary - obj.imaginary;
+        return res;
+    }
 };
+
+template <typename T, typename U>
+ComplexNumber<T, U>::ComplexNumber()
+{
+    if(!std::is_arithmetic_v<T> || !std::is_arithmetic_v<U>) 
+	{
+		throw std::invalid_argument( "non-arithmetic arguments passed" );
+	}
+	
+	this->real = 0;
+	this->imaginary = 0;
+}
 
 template <typename T, typename U>
 ComplexNumber<T, U>::ComplexNumber(T real, U imaginary)
