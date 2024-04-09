4db70a8bb088967097c0f42b3bb7ef5ea62024de
Ganesh Mayya <87609509+thehyena24@users.noreply.github.com>
2024-04-09 10:57:09 +0530

    Updated matrix.h

--- include/matrix.h
+++ include/matrix.h
@@ -16,65 +16,72 @@ public:
     int no_rows;
     int no_cols;
 
-    Matrix()
-    {
-        matrix = new VectorContainer <VectorContainer<T>*>();
-        this->no_rows = rows;
-        this->no_cols = cols;
-
-        int curr_rows = rows;
-
-        while(curr_rows != 0)
-        {
-            VectorContainer <T>* temp = new VectorContainer <T>();
-
-            for(int i = 0; i < cols; i++)
-            {
-                temp->push_back(0);
-            }
-
-            matrix->push_back(temp);
-
-            curr_rows--;
-        }
-    }
-
-    Matrix(std::initializer_list<T> init)
-    {
-        matrix = new VectorContainer <VectorContainer<T>*>();
-        this->no_rows = rows;
-        this->no_cols = cols;
-
-        int curr_rows = rows;
-
-        auto first = init.begin();
-        auto last = init.end();
-
-        while(curr_rows != 0 && first != last)
-        {
-            VectorContainer <T>* temp = new VectorContainer <T>();
-
-            for(int i = 0; i < cols; i++, (void)++first)
-            {
-                temp->push_back(*first);
-            }
-
-            matrix->push_back(temp);
-
-            curr_rows--;
-        }
-    }
-
-    ~Matrix()
-    {
-        for(int i = 0; i < matrix->size(); i++)
-            delete (*matrix)[i];
-
-        delete matrix;
-    }
+    Matrix();
+    Matrix(std::initializer_list<T> init);
+    ~Matrix();
 
     void displayMatrix();
 };
+
+template <typename T, int rows, int cols>
+Matrix<T, rows, cols>::Matrix()
+{
+    matrix = new VectorContainer <VectorContainer<T>*>();
+    this->no_rows = rows;
+    this->no_cols = cols;
+
+    int curr_rows = rows;
+
+    while(curr_rows != 0)
+    {
+        VectorContainer <T>* temp = new VectorContainer <T>();
+
+        for(int i = 0; i < cols; i++)
+        {
+            temp->push_back(0);
+        }
+
+        matrix->push_back(temp);
+
+        curr_rows--;
+    }
+}
+
+template <typename T, int rows, int cols>
+Matrix<T, rows, cols>::Matrix(std::initializer_list<T> init)
+{
+    matrix = new VectorContainer <VectorContainer<T>*>();
+    this->no_rows = rows;
+    this->no_cols = cols;
+
+    int curr_rows = rows;
+
+    auto first = init.begin();
+    auto last = init.end();
+
+    while(curr_rows != 0 && first != last)
+    {
+        VectorContainer <T>* temp = new VectorContainer <T>();
+
+        for(int i = 0; i < cols; i++, (void)++first)
+        {
+            temp->push_back(*first);
+        }
+
+        matrix->push_back(temp);
+
+        curr_rows--;
+    }
+}
+
+template <typename T, int rows, int cols>
+Matrix<T, rows, cols>::~Matrix()
+{
+    for(int i = 0; i < matrix->size(); i++)
+        delete (*matrix)[i];
+
+    delete matrix;
+}
 
 template <typename T, int rows, int cols>
 void Matrix<T, rows, cols>::displayMatrix()
