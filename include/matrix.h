4244b7d80548bae9f9a519d3a991d494a0feed87
Ganesh Mayya <87609509+thehyena24@users.noreply.github.com>
2024-04-09 09:40:51 +0530

    Updated matrix.h

--- include/matrix.h
+++ include/matrix.h
@@ -6,47 +6,70 @@
 #include <type_traits>
 #include <concepts>
 #include <initializer_list>
+#include <iostream>
 
 template <typename T>
 class Matrix
 {
 public:
-    Vector <Vector<T>> matrix;
+    VectorContainer <VectorContainer<T>*>* matrix;
     int no_rows;
     int no_cols;
 
     Matrix()
     {
-        matrix = Vector <Vector<T>>();
+        matrix = new VectorContainer <VectorContainer<T>*>();
         no_rows = 0;
         no_cols = 0;
     }
 
     Matrix(int rows, int cols)
     {
-        matrix = Vector <Vector<T>>();
+        matrix = new VectorContainer <VectorContainer<T>*>();
         this->no_rows = rows;
         this->no_cols = cols;
 
         while(rows != 0)
         {
-            Vector <T> temp;
+            VectorContainer <T>* temp = new VectorContainer <T>();
 
             for(int i = 0; i < cols; i++)
             {
-                temp.push_back(0);
+                temp->push_back(0);
             }
 
-            matrix.push_back(temp);
+            matrix->push_back(temp);
 
             rows--;
         }
     }
 
-    Matrix(int rows, int cols, std::initializer_list<T> init)
+    ~Matrix()
     {
-        
+        for(int i = 0; i < matrix->size(); i++)
+            delete (*matrix)[i];
+
+        delete matrix;
     }
+
+    void displayMatrix()
+    {
+        for(int i = 0; i < this->no_rows; i++)
+        {
+            std::cout << std::endl;
+            VectorContainer <int> currRow = *((*matrix)[i]);
+
+            for(int j = 0; j < this->no_cols; j++)
+                std::cout << currRow[j] << " ";
+
+            std::cout << std::endl;
+        }
+    }
+
+    // Matrix(int rows, int cols, std::initializer_list<T> init)
+    // {
+        
+    // }
 };
 
 #endif
--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,26 @@
 #include "vector.h"
+#include "matrix.h"
 #include <iostream>
 
 int main (int argc, char *argv[]) {
-	Vector<int> x = {1, 2};
-	Vector<float> y = {1.5, 2.5};
+	// Vector<int> x = {1, 2};
+	// Vector<float> y = {1.5, 2.5};
 
-	std::cout << dot(x, y) << std::endl;
-	std::cout << euclidean_distance(x, x) << std::endl;
+	// std::cout << dot(x, y) << std::endl;
+	// std::cout << euclidean_distance(x, x) << std::endl;
 
-	const int a = 10;
+	// const int a = 10;
 
-	x.push_back(a);
-	x.square();
+	// x.push_back(a);
+	// x.square();
 
-	for (int i = 0; i < x.size(); i++) {
-		std::cout << x[i] << std::endl;
-	}
+	// for (int i = 0; i < x.size(); i++) {
+	// 	std::cout << x[i] << std::endl;
+	// }
+
+	Matrix <int> x(2, 2);
+
+	x.displayMatrix();
 
 	return 0;
 }
